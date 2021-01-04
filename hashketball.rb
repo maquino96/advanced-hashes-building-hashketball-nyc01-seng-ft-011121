72b189e7917e48125772310514838cbddaf31175
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 19:43:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,6 @@ def game_hash
       :team_name => "",
       :colors => [],
       :players => [
-        
         { :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 }, 
         
         { :player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 },
@@ -14,14 +13,15 @@ def game_hash
         
         { :player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5 },
         
-        
-        
+        { :player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1 }
         ]
     },
     :away => {
       :team_name => "",
       :colors => [],
-      :players => []
+      :players => [
+        
+        ]
     }
   }
 end
