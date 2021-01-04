e0e1f70701975b55c81b48f7bc25604c050a413c
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 19:39:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,15 @@
 # Write your code here!
 def game_hash
-  {}
+  manual_hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => []
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => []
+    }
+  }
 end
